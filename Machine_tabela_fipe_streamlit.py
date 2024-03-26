1dcce06b8d1bbf74fb551c8c5ccf9e94fda1e361
Rafael Duria Refundini <97250662+rafaelduria@users.noreply.github.com>
2024-03-26 11:12:07 -0300

    Update Machine_tabela_fipe_streamlit.py

--- Machine_tabela_fipe_streamlit.py
+++ Machine_tabela_fipe_streamlit.py
@@ -56,7 +56,7 @@ st.header('Tabela Fipe')
 
 #Barra lateral esquerda
 htp="https://raw.githubusercontent.com/rafaelduria/Machine_Learning_LinearRegression_LogisticRegression_predict_table_fipe_Brazil/blob/main/img_fipe.png"
-st.sidebar.image(htp)
+st.sidebar.image(htp, width=150)
 #titulo
 st.sidebar.title('Filtro')
 #lista marca carros tabela fipe
