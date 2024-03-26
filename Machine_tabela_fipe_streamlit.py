425ac7e7308dd569197025ee660e7323785b00ff
Rafael Duria Refundini <97250662+rafaelduria@users.noreply.github.com>
2024-03-26 11:13:30 -0300

    Update Machine_tabela_fipe_streamlit.py
    svg

--- Machine_tabela_fipe_streamlit.py
+++ Machine_tabela_fipe_streamlit.py
@@ -55,7 +55,7 @@ st.header('Tabela Fipe')
 #)
 
 #Barra lateral esquerda
-htp="https://raw.githubusercontent.com/rafaelduria/Machine_Learning_LinearRegression_LogisticRegression_predict_table_fipe_Brazil/blob/main/img_fipe.png"
+htp="https://raw.githubusercontent.com/rafaelduria/Machine_Learning_LinearRegression_LogisticRegression_predict_table_fipe_Brazil/blob/main/img_fipe.svg.png"
 st.sidebar.image(htp, width=150)
 #titulo
 st.sidebar.title('Filtro')
