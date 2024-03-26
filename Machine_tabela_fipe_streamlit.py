14145aa9dcf53746925f33eb948dc46c19a2e8e6
Rafael Duria Refundini <97250662+rafaelduria@users.noreply.github.com>
2024-03-26 08:11:40 -0300

    Update Machine_tabela_fipe_streamlit.py

--- Machine_tabela_fipe_streamlit.py
+++ Machine_tabela_fipe_streamlit.py
@@ -53,7 +53,7 @@ st.markdown(
     """
     <style>
         section[data-testid="stSidebar"] {
-            width: 500px !important; # Set the width to your desired value
+            width: 300px !important; # Set the width to your desired value
         }
     </style>
     """,
